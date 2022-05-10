3ce7483cbe90e5ab3e71261509f676a831a95dd9
andremw96 <andre19961@gmail.com>
2022-05-10 10:00:59 +0700

    show log

--- app/src/main/java/com/example/andre/cchat/view/setting/SettingsActivity.java
+++ app/src/main/java/com/example/andre/cchat/view/setting/SettingsActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -165,7 +166,7 @@ public class SettingsActivity extends AppCompatActivity {
                             .setQuality(50)
                             .compressToBitmap(thumb_filePathUri);
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    Log.e("error compress", e.getMessage());
                 }
 
                 ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
