8c9247465824fc8a304817daddcd2d35037b8602
andremw96 <andre19961@gmail.com>
2022-05-17 11:52:12 +0700

    add new unit test

--- app/src/test/java/com/example/andre/cchat/TextHelperTest.kt
+++ app/src/test/java/com/example/andre/cchat/TextHelperTest.kt
@@ -1,12 +1,13 @@
 package com.example.andre.cchat
 
 import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
 
 class TextHelperTest {
 
-    private lateinit var textHelper: TextHelper
+    private var textHelper: TextHelper? = null
 
     @Before
     fun setup() {
@@ -16,7 +17,15 @@ class TextHelperTest {
     @Test
     fun `Text Helper getText should return low if 5`() {
         val expected = "low"
-        val result = textHelper.getText(5)
+        val result = textHelper?.getText(5)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `TextHelper getText should return high if 100`() {
+        val expected = "high"
+        val result = textHelper?.getText(100)
 
         assertEquals(expected, result)
     }
